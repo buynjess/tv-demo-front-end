588437a3a0f181b0438fdf7a848f33addeab09b9
Jessica Buynak <buynjess31@gmail.com>
2019-01-30 19:10:43 -0700

    finished part 10

--- src/PreviewPage.js
+++ src/PreviewPage.js
@@ -1,74 +1,86 @@
-import React, { Component } from "react"
+import React, {
+    Component
+} from "react"
 import TvShow from "./TvShow"
 import propTypes from 'prop-types'
 
-export default class PreviewPage extends Component{
-    
+export default class PreviewPage extends Component {
+
     tvShowSelected = () => {
         this.setState({
-            selectedShow:{
-                name:this.props.tvShow.name, 
-                rating:this.props.tvShow.rating, 
-                img:this.props.tvShow.img}
+            selectedShow: {
+                name: this.props.tvShow.name,
+                rating: this.props.tvShow.rating,
+                img: this.props.tvShow.img
+            }
         })
-    }    
+    }
     static propTypes = {
         tvShow: propTypes.object.isRequired,
         tvShows: propTypes.array.isRequired
     }
 
     state = {
-        selectedShow: {name:"", rating:"", img:""}
+        selectedShow: {
+            name: "",
+            rating: "",
+            img: ""
+        }
     }
 
     renderTvShow = () => {
         // console.log('from renderTvShowOnManagePage', this.props.tvShows)
-            let showsArray = this.props.tvShows;
-            return showsArray.filter((show)=>show.rating < 4
-            ).map((show) => {
-                return (
-                <TvShow name={show.name}
-                        allowDelete={true}
-                        selectHandler={this.tvShowSelected}
-                        deleteHandler={this.tvShowDeleted}
-                        saveHandler={this.saveTvShow}
-                        tvShows={show}/>
-                        )
-            })
-        }
-        calculateAvgRating = () => {
-            console.log(this.props.tvShows)
-            this.props.tvShows.map((e)=>{
-                return e.rating
-            }).reduce((acc, currval) => {
-               console.log(acc + currval) 
-            })
-            //calculate the ave rateing of all tvshows
+        let showsArray = this.props.tvShows;
+        return showsArray.filter((show) => show.rating < 4).map((show) => {
+            return ( <
+                TvShow name = {
+                    show.name
+                }
+                allowDelete = {
+                    true
+                }
+                selectHandler = {
+                    this.tvShowSelected
+                }
+                deleteHandler = {
+                    this.tvShowDeleted
+                }
+                saveHandler = {
+                    this.saveTvShow
+                }
+                tvShows = {
+                    show
+                }
+                />
+            )
+        })
+    }
+    calculateAvgRating = () => {
+        console.log(this.props.tvShows)
+        return this.props.tvShows.reduce((acc, curval) => {
+            console.log(acc.rating)
+            console.log(curval.rating)
+            return (acc + parseInt(curval.rating)/this.props.tvShows.length)
+        },0);
+    }
 
-                //sum the values of values from the array
-                //get length of array
-                //divide array by sum of length
-            //return 0 when no tvshows
-            //return the rating of single tvshow 
-
-        }
-
-    // renderTvShow = () => {
-    //     console.log(this.props.tvshows)
-    //     // const showsArray = this.props.tvShows;
-    //     // const rateingFilter = rate.filter(rate => rate <= 4);
-    
-    //     return(
-    //         <TvShow name={this.props.tvShow.name} selectHandler={this.props.tvShowSelected}/>
-    //     )
+    // calculateAvgRating = () => {
+    //     console.log(this.props.tvShows)
+    //     this.props.tvShows.map((a) => {
+    //         console.log(a.rating)
+    //         return a.rating
+    //     }).reduce((acc, curval) => {
+    //         console.log(acc + curval)
+    //     },0);
     // }
-    render(){
-        this.calculateAvgRating()
+
+    render = () => {
         return(
             <div>
                 <div className = "lower-container">
                     <aside>
                          <h2>Shows</h2>
+                         {this.calculateAvgRating()}
                          {this.renderTvShow()}
                     </aside>
                     <section>
@@ -78,5 +90,5 @@ export default class PreviewPage extends Component{
             </div>
 
         )
-    }
+    }   
 }
